324d4f3f2ef37672e42f10f241be431b57417011
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-07-17 13:39:45 +0600

    made Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className=''>
+            <div className='header'>
+                <h1>Learning Portal</h1>
+                <img src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80" alt="" />
+            </div>
+            <hr />
+        </div>
+        
+    );
+};
+
+export default Header;
